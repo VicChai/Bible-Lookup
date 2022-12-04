f2cab67ac191b9720e92f103cbdf98ddadd7ccab
Victor Chai <Vic.P.Chai@gmail.com>
2022-12-03 23:47:35 -0500

    changed to make responsive, also added space under last verse

--- main.js
+++ main.js
@@ -9,23 +9,27 @@ async function getFetch() {
     let data = await bible.json();
     let book = document.createElement("div");
     book.classList.add("text-2xl");
-    book.classList.add("ml-80");
+    book.classList.add("text-center");
     book.classList.add("mb-5");
     book.classList.add("mt-6");
     book.innerText = data.verses[0].book_name;
     document.body.appendChild(book);
     let displas = document.createElement("div");
-    displas.classList.add("ml-80");
+    displas.classList.add("text-center");
     displas.classList.add("mb-8");
     displas.innerText = `Chapter ${data.verses[0].chapter}`;
     document.body.appendChild(displas);
 
     for (let i = 0; i < data.verses.length; i++) {
       let displaText = document.createElement("div");
-      displaText.classList.add("ml-10");
+      displaText.classList.add("ml-5");
+      displaText.classList.add("mr-5");
       displaText.innerText += `${data.verses[i].verse} ${data.verses[i].text}`;
       document.body.appendChild(displaText);
     }
+    let space = document.createElement("div");
+    space.classList.add("my-10");
+    document.body.appendChild(space);
   } catch (err) {
     console.log(`error ${err}`);
   }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./Bible API/**/*.{html,js}"],
+  content: ["./**/*.{html,js}"],
   theme: {
     extend: {},
   },
